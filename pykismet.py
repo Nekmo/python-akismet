12b1786e3550383c3e46f0219524ab95f38d3310
George Goldberg <george@grundleborg.com>
2014-04-08 21:35:28 +0100

    Add support for the API's submitting ham and spam features.

--- pykismet.py
+++ pykismet.py
@@ -5,6 +5,8 @@ AKISMET_USER_AGENT = "Pykismet/0.0.0"
 
 # API URIs
 AKISMET_CHECK_URL = "rest.akismet.com/1.1/comment-check"
+AKISMET_SUBMIT_SPAM_URL = "rest.akismet.com/1.1/submit-spam"
+AKISMET_SUBMIT_HAM_URL = "rest.akismet.com/1.1/submit-ham"
 
 # API Permitted parameter lists
 AKISMET_CHECK_VALID_PARAMETERS = {
@@ -25,8 +27,14 @@ AKISMET_CHECK_VALID_PARAMETERS = {
         'is_test',
 }
 
+AKISMET_SUBMIT_SPAM_VALID_PARAMETERS = AKISMET_CHECK_VALID_PARAMETERS
+AKISMET_SUBMIT_HAM_VALID_PARAMETERS = AKISMET_CHECK_VALID_PARAMETERS
+
 # Custom Exceptions
 class AkismetError(Exception):
+    pass
+
+class InternalPykismetError(AkismetError):
     pass
 
 class MissingApiKeyError(AkismetError):
@@ -87,6 +95,59 @@ class Akismet:
             return False
         elif r.text == "true":
             return True
+        else:
+            raise AkismetServerError("Akismet server returned an error: "+r.text)
+    def submit_spam(self, parameters):
+        self.submit("spam", parameters)
+
+    def submit_ham(self, parameters):
+        self.submit("ham", parameters)
+
+    def submit(self, t, parameters):
+
+        # Check if the API key is set
+        if self.api_key is None:
+            raise MissingApiKeyError("api_key must be set on the akismet object before calling any API methods.")
+
+        # Throw appropriate exception if any mandatory parameters are missing
+        if not 'blog' in parameters:
+            if self.blog_url is None:
+                raise MissingParameterError("blog is a required parameter if blog_url is not set on the akismet object")
+            parameters['blog'] = self.blog_url
+        if not 'user_ip' in parameters:
+            raise MissingParameterError("user_ip is a required parameter")
+        if not 'user_agent' in parameters:
+            raise MissingParameterError("user_agent is a required parameter")
+
+        # Check for any invalid extra parameters
+        if t is "spam":
+            leftovers = set(parameters.keys()).difference_update(AKISMET_SUBMIT_SPAM_VALID_PARAMETERS)
+        elif t is "ham":
+            leftovers = set(parameters.keys()).difference_update(AKISMET_SUBMIT_HAM_VALID_PARAMETERS)
+        else:
+            raise InternalPykismetError("submit called with invalid t")
+
+        if leftovers and leftovers.count() != 0:
+            raise ExtraParametersError("The following unrecognised parameters were supplied:"+str(leftovers))
+
+        # Build the HTTP Headers for the Akismet query
+        headers = {
+                "User-Agent": AKISMET_USER_AGENT,
+        }
+
+        # Construct the GET request to akismet.
+        if t is "spam":
+            url = "http://"+self.api_key+"."+AKISMET_SUBMIT_SPAM_URL
+        elif t is "ham":
+            url = "http://"+self.api_key+"."+AKISMET_SUBMIT_HAM_URL
+        else:
+            raise InternalPykismetError("submit called with invalid t")
+
+        r = requests.post(url, data=parameters, headers=headers)
+
+        # Deal with the response
+        if r.text == "Thanks for making the web a better place.":
+            return
         else:
             raise AkismetServerError("Akismet server returned an error: "+r.text)
 
